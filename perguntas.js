78c124bae23be3c543f6ca1822a9a75f32bbcb70
Elisandrinho <106559520+Elisandrinho@users.noreply.github.com>
2024-10-10 08:26:12 -0300

    .

---
+++ perguntas.js
@@ -0,0 +1,107 @@
+criaCartao(
+    'história',
+    ' Quem foi Dom Pedro I?',
+    'O primeiro imperador do Brasil, conhecido por declarar a independência em 1822')
+
+criaCartao(
+    'historia',
+    'Qual foi a causa da Revolução Francesa?',
+    'Desigualdade social, crise econômica e influência das ideias iluministas')
+
+
+
+criaCartao(
+    'história',
+    ' que aconteceu em 1964 no Brasil?',
+    'Um golpe militar que depôs o presidente João Goulart.')
+
+
+criaCartao(
+    'história',
+    'Quem foi Mahatma Gandhi?',
+    'Líder indiano que lutou pela independência da Índia através da não-violência.')
+
+
+criaCartao(
+    'História',
+    'Qual foi a importância da Revolução Industrial?',
+    'Transformou economias agrárias em industriais, mudando a sociedade e o trabalho.')
+
+
+criaCartao(
+    'Biologia',
+    'O que é fotossíntese?',
+    'Processo em que plantas convertem luz solar em energia química, usando água e dióxido de carbono.')
+
+
+criaCartao(
+    'Geografia',
+    'Quais são os principais grupos de rochas?',
+    'Ígneas, sedimentares e metamórficas.')
+
+
+criaCartao(
+    'Química',
+    'O que define uma reação química?',
+    'Transformação de substâncias em outras, com quebra e formação de ligações químicas.')
+
+criaCartao(
+    'Fisíco-química',
+    'Quais são os estados da matéria?',
+    'Sólido, líquido, gasoso e plasma.')
+
+criaCartao(
+    'Química',
+    'O que é a tabela periódica?',
+    'Organização dos elementos químicos de acordo com suas propriedades e números atômicos.')
+
+criaCartao(
+    'Geografia',
+    'Quais são os principais biomas do Brasil?',
+    'Amazônia, Cerrado, Caatinga, Mata Atlântica, Pampas e Pantanal.')
+
+criaCartao(
+    'Geografia',
+    'O que é um fuso horário?',
+    'Divisão da Terra em regiões que têm a mesma hora.')
+
+criaCartao(
+    'Geografia',
+    'Qual é a capital da Austrália?',
+    'Canberra')
+
+criaCartao(
+    'Geografia',
+    'O que caracteriza um clima tropical?',
+    'Altas temperaturas e chuvas abundantes durante o ano.')
+
+criaCartao(
+    'Biologia',
+    'Qual é o maior rio do mundo?',
+    'O rio Amazonas.')
+
+
+criaCartao(
+    'Língua Portuguesa',
+    'O que é uma metáfora?',
+    'Comparação implícita entre duas coisas diferentes, sem usar "como".')
+
+criaCartao(
+    'Língua Portuguesa',
+    'Quais são os tipos de crase?',
+    'Crase obrigatória (artigo + pronome) e crase facultativa (duas vogais iguais).')
+
+criaCartao(
+    'Língua Portuguesa',
+    'O que é um advérbio?',
+    'Palavra que modifica um verbo, adjetivo ou outro advérbio.')
+
+criaCartao(
+    'Língua Portuguesa',
+    'Qual é a diferença entre gênero e número?',
+    'Gênero se refere à categoria (masculino ou feminino) e número ao singular ou plural.')
+
+criaCartao(
+    'Língua Portuguesa',
+    'O que é um poema?',
+    'Composição literária que expressa emoções, ideias ou histórias, muitas vezes em versos.')
